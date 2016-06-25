3192c1c6e740f940de8baa441e9fc0cc6db3e51c
MSHERMAN <mike.sher08@gmail.com>
2016-06-25 16:25:34 -0500

    Automate navigation building

---
+++ js/nav.js
@@ -0,0 +1,61 @@
+var nav = {
+	"days": [{
+		"name": "Day 1",
+		"activities": ["Driving"],
+		"destination": "Horse Cave, KY"
+	},
+	{
+		"name": "Day 2",
+		"activities": ["Mammoth Caves", "Nashville"],
+		"destination": "Nashville, TN"
+	},
+	{
+		"name": "Day 3",
+		"activities": ["Cades Cove drive", "Abrams Falls", "Blue Ridge Parkway"],
+		"destination": "Gatlinburg, TN"
+	},
+	{
+		"name": "Day 4",
+		"activities": ["Chimney Tops", "Groto Falls", "Food Place"],
+		"destination": "Asheville, NC"
+	},
+	{
+		"name": "Day 5",
+		"activities": ["Biltmore Estate", "Sierra Nevada Brewing"],
+		"destination": "Asheville, NC"
+	},
+	{
+		"name": "Day 6",
+		"activities": ["Grandfather Mountain", "Linville Falls", "Food Place"],
+		"destination": "Floyd, VA"
+		
+	},
+	{
+		"name": "Day 7",
+		"activities": ["Monticello", "Trans-Allegheny Lunatic Asylum"],
+		"destination": "Clarksburg, WV"
+	},
+	{
+		"name": "Day 8",
+		"activities": ["Driving"],
+		"destination": "Green Bay, WI"
+	}],
+	display: function(){
+		var dayTemplate = "<div class='dayNav'>%1</div>";
+		var activityTemplate = "<div class='activityNav'>%1</div>";
+		var dayDetails = "";
+		var dayName = "";
+		var activityName = "";
+		for (day in nav.days){
+			dayDetails = nav.days[day];
+			var dayName = dayTemplate.replace("%1", dayDetails.name);
+			$(".nav").append(dayName);
+			for (activity in nav.days[day].activities){
+				var activityName = activityTemplate.replace("%1", dayDetails.activities[activity]);
+				$(".nav").append(activityName);
+			}
+		}
+	}
+}
+
+nav.display();
